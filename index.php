cbed68b4a3938279dfe0b5e2274705613f476d00
unknown <delamoragr129@gmail.com>
2022-10-26 16:20:24 -0300

    update files new idea

--- index.php
+++ index.php
@@ -10,29 +10,7 @@
     </head>
     <body>
         <div class="main-container">
-            
-            <?php include 'header.view.php'; ?>
-             
-           <div class="container-fluid">
-                <div class="row">
-                    <section>
-                        <div class="cont">
-                            <h1>Bienvenido CopySchool!</h1>
-                            <h3>Uno de los tantos sitios intentando hacer que la programación sea un poco más fácil de entender. </h3>
-                            <br>
 
-                        </div>
-                    </section>
-                </div>
-            </div>
-            <div class="container-fluid">
-                <div class="row">
-                    <div class="cont tips">
-                        <p>Esto que ves aquí debajo es un formulario. Y al hacer <a href="#cont_singUp">Click</a> en este hipervinculo te llevara al formulario de registro porque porque es a donde apunta el mismo.</p>
-                        <p>Lo usaremos para poder ingresar nuestros datos y que queden almacenados en la base de datos, para más adelante poder manipular los mismos.</p>
-                    </div>
-                </div>
-            </div>
             <div class="container">
                 <div class="row">
                     <section class="section" id="cont_singUp">
@@ -49,34 +27,6 @@
                                 <input type="text" name="userName" placeholder="Usuario" autocomplete="off" required>
                                 <input type="password" name="password" placeholder="Password" maxlength="10" required>
                                 <input type="submit" value="Registrarme">
-                            </form>
-                        </div>
-                    </section>
-                </div>
-            </div>
-            <div class="container-fluid">
-                <div class="row">
-                    <div class="separator"></div>
-                </div>
-            </div>
-            <div class="container">
-                <div class="row">
-                    <section class="section">
-                        <div>
-                            <h3>Formulario 2</h3>
-                        </div>
-                        <div class="cont cont-form">
-                            <form>
-                                <input type="number" name="value1" placeholder="Cifra 1">
-                                <button class="btn btn-danger">+</button>
-                                <button class="btn btn-danger">-</button>
-                                <button class="btn btn-danger">x</button>
-                                <button class="btn btn-danger">/</button>
-                                <input type="number" name="value2" placeholder="Cifra">
-                                <br>
-                                <button class="btn btn-success">=</button>
-                                <br>
-                                <textarea></textarea>
                             </form>
                         </div>
                     </section>
