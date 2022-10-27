8aa6268fff07d7e84bdf2634ee7b0bcdc41da2f5
unknown <delamoragr129@gmail.com>
2022-10-27 14:51:32 -0300

    upLoad files for mvc

--- index.php
+++ index.php
@@ -6,20 +6,19 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="css/bootstrap.min.css" rel="stylesheet">
         <link href="css/general.css" rel="stylesheet">
-        <link href="css/header-navs-menus.css" rel="stylesheet">
     </head>
     <body>
 
         <div class="container">
-            <div class="row justify-content-center align-items-center">
-                <div class="">
-                    <div class="cont-form">
-                        <form>
-                            <input type="text" name="userName" placeholder="User" autocomplete="off" required>
-                            <input type="password" name="password" placeholder="Password" autocomplete="off" required>
-                            <input type="submit" class="btn btn-success" value="Login">
-                        </form>
-                    </div>
+            <div class="row cont-index">
+                <div class="cont-form">
+                    <form>
+                        <label>User</label>
+                        <input type="text" name="userName" placeholder="Write your user-name here" autocomplete="off" required>
+                        <label>Password</label>
+                        <input type="password" name="password" placeholder="Write your password here" autocomplete="off" required>
+                        <input type="submit" class="btn btn-success" value="Login">
+                    </form>
                 </div>
             </div>
         </div>
