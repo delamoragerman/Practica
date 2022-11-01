9376a7fa5ca7280d44136aec7f63cb2a3bb41e65
unknown <delamoragr129@gmail.com>
2022-11-01 12:59:00 -0300

    file update

--- mvc/views/footer_js.view.php
+++
@@ -1,7 +0,0 @@
-<footer>
-    <script src="/js/jquery-3.6.0.min.js"></script>
-    <script src="https://kit.fontawesome.com/c2d935188e.js" crossorigin="anonymous"></script>
-    <script src="js/bootstrap.min.js"></script>            
-    <script src="js/general.js" type="text/javascript"></script>
-</footer>
-</div>
